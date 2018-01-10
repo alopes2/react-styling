466cdd59cd4dc1369014c890d28a0c62ad05e759
Andre Lopes <andrevitorlopes@gmail.com>
2018-01-10 21:43:52 -0200

    importing css files as 'classes ' and assigning them to the components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from  './App.css';
 // import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
@@ -84,18 +84,18 @@ class App extends Component {
         ;
     }
 
-    const classes = [];
+    const assignedClasses = [];
 
     if(this.state.persons.length <= 2)
-      classes.push('red')
+      assignedClasses.push(classes.red)
     if(this.state.persons.length <= 1)
-      classes.push('bold')
+      assignedClasses.push(classes.bold)
 
     return (
       /* <StyleRoot> */
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm a React app!</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
           <button 
               style={style}
               onClick={this.togglePersonsHandler}
