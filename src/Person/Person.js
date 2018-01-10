ede33164fcf8bd69cabe558dfb296ee954155d89
Andre Lopes <andrevitorlopes@gmail.com>
2018-01-10 21:09:53 -0200

    adding @media-queries and wrapping the application in StyleRoot

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,8 +5,13 @@ import Radium from 'radium';
 const person = (props) => {
     // When using class-based components, it`s this.props
     // like <p>My name is {this.props.name}</p>
+    const style = {
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    }
     return (
-        <div className="Person">
+        <div className="Person" style={style}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.change} value={props.name}/>
